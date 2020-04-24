840cdbd06b9cc4d11e5c3997b25388bf6d9ac007
oussamabng <47861021+oussamabng@users.noreply.github.com>
2020-04-24 15:41:11 +0100

    prepare env (#2)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//? import screens
+
+//? import slick css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/admin" component={() => <div>admin</div>} />
+        <Route path="/login" component={() => <div>login</div>} />
+        <Route path="/signup" component={() => <div>signup</div>} />
+      </Switch>
+    </Router>
   );
 }
 
