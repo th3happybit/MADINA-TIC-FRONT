f0245004a91e45fe04d5127d9a9ea21f603a96fe
oussamabng <o.bengoudifa@esi-sba.dz>
2020-04-27 22:56:28 +0100

    implement admin profile design

--- frontend/src/components/AdminEditProfile/AdminEditProfile.jsx
+++ frontend/src/components/AdminEditProfile/AdminEditProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Form, Input, Button } from "semantic-ui-react";
 
 //? import css
 import "./AdminEditProfile.css";
@@ -26,6 +26,97 @@ const AdminEditProfile = () => {
             onClick={handleItemClick}
           />
         </Menu>
+        <div className="_form_profile_informations">
+          {activeItem === "info" && (
+            <Form className="_margin_vertical_lg">
+              <Form.Group widths="equal">
+                <Form.Field
+                  id="form-input-control-first-name"
+                  control={Input}
+                  label="First name"
+                  placeholder="First name"
+                />
+                <Form.Field
+                  id="form-input-control-last-name"
+                  control={Input}
+                  label="Last name"
+                  placeholder="Last name"
+                />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Field
+                  id="form-input-control-email"
+                  control={Input}
+                  label="Email"
+                  placeholder="Email"
+                />
+                <Form.Field
+                  id="form-input-control-birthday"
+                  control={Input}
+                  label="Birthday"
+                  placeholder="Birthday"
+                />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Field
+                  id="form-input-control-address"
+                  control={Input}
+                  label="Address"
+                  placeholder="Address"
+                />
+                <Form.Field
+                  id="form-input-control-phone"
+                  control={Input}
+                  label="Phone Number"
+                  placeholder="Phone Number"
+                />
+              </Form.Group>
+              <Form.Group widths="equal">
+                <Form.Field
+                  id="form-input-control-fcb"
+                  control={Input}
+                  label="Facebok link"
+                  placeholder="Facebok link"
+                />
+                <Form.Field
+                  id="form-input-control-google"
+                  control={Input}
+                  label="Google link"
+                  placeholder="Google link"
+                />
+              </Form.Group>
+            </Form>
+          )}
+          {activeItem === "password" && (
+            <Form className="_margin_vertical_lg">
+              <Form.Group className="_form_password">
+                <Form.Input
+                  type="texte"
+                  label="Current Password"
+                  placeholder="Current Password"
+                />
+              </Form.Group>{" "}
+              <Form.Group className="_form_password">
+                <Form.Input
+                  type="texte"
+                  label="New Password"
+                  placeholder="New Password"
+                />
+              </Form.Group>{" "}
+              <Form.Group className="_form_password">
+                <Form.Input
+                  type="texte"
+                  label="Confirm Password"
+                  placeholder="Confirm Password"
+                />
+              </Form.Group>{" "}
+            </Form>
+          )}
+          <div className="_button_edit_profile">
+            <Button className="button_secondary ">Cancel</Button>
+            <Button className="button_primary ">Save</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
