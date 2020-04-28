6c8e9ac2e2bcfd76ed0aab61bf0fa2d755350780
oussamabng <o.bengoudifa@esi-sba.dz>
2020-04-28 05:01:48 +0100

    implment design login admin

--- frontend/src/screens/AdminLogin/AdminLogin.jsx
+++ frontend/src/screens/AdminLogin/AdminLogin.jsx
@@ -1,21 +1,41 @@
 import React from "react";
-import { Container, Grid } from "semantic-ui-react";
-import FormLogin from "../../components/FormLogin/FormLogin.jsx"
-import "../../index.css"
-import "./AdminLogin.css"
+import { Form, Button, Container } from "semantic-ui-react";
+import "./AdminLogin.css";
 
+import { ReactComponent as Logo } from "../../assets/images/madinatic_logo.svg";
 
-const AdminLogin = () =>  {
- return(
-  <>
-  <Container className="_admin_login">
-   <Grid className="h-full">
-    <Grid.Column className="p-0 bg-default h-full" width={15}>
-     <FormLogin />
-    </Grid.Column>
-   </Grid>
-  </Container>
-  </>
- );
+const AdminLogin = () => {
+  return (
+    <div className="_admin_login">
+      <Container>
+        <div className="_admin_card">
+          <div className="row">
+            <Logo className="_logo" />
+            <p className="title text-active bold ">MADINA TIC</p>
+            <Form className="_admin_login_form _margin_vertical_md">
+              <Form.Input
+                placeholder="Email or username"
+                type="text"
+                size="large"
+                className="_margin_vertical_sm small"
+              />
+              <Form.Input
+                placeholder="Password"
+                type="password"
+                size="large"
+                className="_margin_vertical_sm small"
+              />
+              <Button
+                className="button_primary _margin_vertical_md"
+                type="submit"
+              >
+                Login
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 export default AdminLogin;
