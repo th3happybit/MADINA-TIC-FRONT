30994628b684645b0b13ee737900b2ebb7c7da3a
oussamabng <o.bengoudifa@esi-sba.dz>
2020-04-28 05:05:47 +0100

    fix a bug

--- frontend/src/components/FormLogin/MainForm.jsx
+++ frontend/src/components/FormLogin/MainForm.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Form, Button} from "semantic-ui-react";
+import React from "react";
+import { Form, Button } from "semantic-ui-react";
 
-const MainForm = ()=>{
-    return(
-    <Logo className="_logo" />
-    <p className="title text-active bold ">MADINA TIC</p>
-    <Form className="_admin_login_form _margin_vertical_md">
-     <Form.Input
-      placeholder="Email or username"
-      type="text"
-      size="large"
-      className="_margin_vertical_sm small"
-     />
-     <Form.Input
-      placeholder="Password"
-      type="password"
-      size="large"
-      className="_margin_vertical_sm small"
-     />
-     <Button
-      className="button_primary _margin_vertical_md"
-      type="submit"
-     >
-      Login
-             </Button>
-    </Form>
-    )
-}
+//? import logo
+import { ReactComponent as Logo } from "../../assets/images/madinatic_logo.svg";
+
+const MainForm = () => {
+  return (
+    <>
+      <Logo className="_logo" />
+      <p className="title text-active bold ">MADINA TIC</p>
+      <Form className="_admin_login_form _margin_vertical_md">
+        <Form.Input
+          placeholder="Email or username"
+          type="text"
+          size="large"
+          className="_margin_vertical_sm small"
+        />
+        <Form.Input
+          placeholder="Password"
+          type="password"
+          size="large"
+          className="_margin_vertical_sm small"
+        />
+        <Button className="button_primary _margin_vertical_md" type="submit">
+          Login
+        </Button>
+      </Form>
+    </>
+  );
+};
 export default MainForm;
--- frontend/src/screens/AdminLogin/AdminLogin.jsx
+++ frontend/src/screens/AdminLogin/AdminLogin.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Form, Button, Container } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import "./AdminLogin.css";
 
-import { ReactComponent as Logo } from "../../assets/images/madinatic_logo.svg";
+//? import component
+import MainForm from "../../components/FormLogin/MainForm.jsx";
 
 const AdminLogin = () => {
   return (
@@ -10,28 +11,7 @@ const AdminLogin = () => {
       <Container>
         <div className="_admin_card">
           <div className="row">
-            <Logo className="_logo" />
-            <p className="title text-active bold ">MADINA TIC</p>
-            <Form className="_admin_login_form _margin_vertical_md">
-              <Form.Input
-                placeholder="Email or username"
-                type="text"
-                size="large"
-                className="_margin_vertical_sm small"
-              />
-              <Form.Input
-                placeholder="Password"
-                type="password"
-                size="large"
-                className="_margin_vertical_sm small"
-              />
-              <Button
-                className="button_primary _margin_vertical_md"
-                type="submit"
-              >
-                Login
-              </Button>
-            </Form>
+            <MainForm />
           </div>
         </div>
       </Container>
