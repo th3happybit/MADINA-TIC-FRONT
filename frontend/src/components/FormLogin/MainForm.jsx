6de8d77a78fddd98132fd0d7fb5dbb247c686269
oussamabng <o.bengoudifa@esi-sba.dz>
2020-04-29 17:21:04 +0100

    add login function fetch api

--- frontend/src/components/FormLogin/MainForm.jsx
+++ frontend/src/components/FormLogin/MainForm.jsx
@@ -1,28 +1,81 @@
-import React from "react";
-import { Form, Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Form, Button, Message } from "semantic-ui-react";
+import axios from "axios";
 
 //? import logo
 import { ReactComponent as Logo } from "../../assets/images/madinatic_logo.svg";
 
 const MainForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, seterror] = useState(null);
+
+  //? function for changing data in inputs
+  const handleChangeInput = (e) => {
+    if (error) seterror(false);
+    let id = e.currentTarget.id;
+    switch (id) {
+      case "username":
+        setUsername(e.currentTarget.value);
+        break;
+      case "password":
+        setPassword(e.currentTarget.value);
+        break;
+      default:
+        break;
+    }
+  };
+  const Login = () => {
+    console.log("login");
+    let axiosConfig = {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    axios
+      .post(
+        "http://13.92.195.8/api/admin/",
+        { username, password },
+        axiosConfig
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <Logo className="_logo" />
       <p className="title text-active bold ">MADINA TIC</p>
-      <Form className="_admin_login_form _margin_vertical_md">
+      <Form error={error} className="_admin_login_form _margin_vertical_md">
         <Form.Input
-          placeholder="Email or username"
+          id="username"
+          onChange={handleChangeInput}
+          placeholder="username"
           type="text"
           size="large"
           className="_margin_vertical_sm small"
+          value={username}
         />
         <Form.Input
+          id="password"
+          onChange={handleChangeInput}
           placeholder="Password"
           type="password"
           size="large"
           className="_margin_vertical_sm small"
+          value={password}
         />
-        <Button className="button_primary _margin_vertical_md" type="submit">
+        <Message error content="Please make sur that all the data is valid" />
+        <Button
+          onClick={Login}
+          className="button_primary _margin_vertical_md"
+          type="submit"
+        >
           Login
         </Button>
       </Form>
--- frontend/src/components/TableNewAccounts/TableNewAccounts.jsx
+++ frontend/src/components/TableNewAccounts/TableNewAccounts.jsx
@@ -7,6 +7,25 @@ import "./TableNewAccounts";
 //? import image
 import Alex from "../../assets/images/alex.jpg";
 
+const data = [
+  {
+    picture: Alex,
+    first_name: "Bengoudifa",
+    last_name: "Oussama",
+    email: "o.bengoudifa@esi-sba.dz",
+    address: "Mostaganem , rue 400 logts",
+    role: "citoyen",
+  },
+  {
+    picture: Alex,
+    first_name: "Messabih",
+    last_name: "Oussama",
+    email: "o.messabih@esi-sba.dz",
+    address: "Chlef , rue 1000 logts",
+    role: "maire",
+  },
+];
+
 const TableNewAccounts = () => {
   return (
     <Table basic="very" striped className="new_accounts_table">
@@ -46,31 +65,6 @@ const TableNewAccounts = () => {
       </Table.Header>
 
       <Table.Body>
-        <Table.Row>
-          <Table.Cell className="medium-text text-default ">
-            <div className="fullname_new_account">
-              <Image src={Alex} className="_new_account-img" />
-              <p className="medium-text text-default table_element">
-                Bensaber djamel
-              </p>
-            </div>
-          </Table.Cell>
-          <Table.Cell className="medium-text text-default ">
-            <p className="table_element"> o.bengoudifa@es-sba.dz</p>
-          </Table.Cell>
-          <Table.Cell className="medium-text text-default element_hide_mobile">
-            <p className="table_element"> Mostaganem,rue 400 logts</p>
-          </Table.Cell>
-          <Table.Cell className="medium-text text-default ">
-            <p className="table_element">Citoyens</p>
-          </Table.Cell>{" "}
-          <Table.Cell className="medium-text text-default ">
-            {" "}
-            <Button className="button_primary btn_account_detail">
-              Account Details
-            </Button>
-          </Table.Cell>
-        </Table.Row>
         <Table.Row>
           <Table.Cell className="medium-text text-default ">
             <div className="fullname_new_account">
