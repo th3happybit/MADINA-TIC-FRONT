a5ea14e2fb0137289fbe8fd568a94f67c2be889f
oussamabng <o.bengoudifa@esi-sba.dz>
2020-05-01 20:31:33 +0100

    some changes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import profile from "./screens/CitoyenProfile/CitoyenProfile.jsx"
 
-
 //? import screens
 import Admin from "./screens/Admin/Admin.jsx";
 import AdminProfile from "./screens/AdminProfile/AdminProfile.jsx";
