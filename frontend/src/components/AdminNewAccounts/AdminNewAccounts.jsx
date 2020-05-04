409268d4890f64b35cb771e18db9e159e5160811
oussamabng <o.bengoudifa@esi-sba.dz>
2020-05-04 16:55:42 +0100

    fix some bugs

--- frontend/src/components/AdminNewAccounts/AdminNewAccounts.jsx
+++ frontend/src/components/AdminNewAccounts/AdminNewAccounts.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Segment } from "semantic-ui-react";
+import { Segment, Search } from "semantic-ui-react";
 import axios from "axios";
 
 //? import css
@@ -12,7 +12,47 @@ const AdminNewAccounts = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [count, setCount] = useState(null);
+  const [searchValue, setsearchValue] = useState("");
+  const [searchLoading, setSearchLoading] = useState(false);
 
+  const handleSearch = (e, { value }) => {
+    setsearchValue(value);
+    searchRequest();
+  };
+
+  const searchRequest = () => {
+    if (searchValue.length > 3 || searchValue.length === 1) {
+      setSearchLoading(true);
+      axios
+        .create({
+          headers: {
+            get: {
+              "Content-Type": "application/json",
+              Authorization: `Token ${localStorage.getItem("admin_token")}`,
+            },
+          },
+        })
+        .request({
+          url: `http://13.92.195.8/api/users/?search=${searchValue}?is_approved=false`,
+          method: "get",
+        })
+        .then((res) => {
+          if (res.count <= 5) {
+            setCount(res.data.count);
+          } else {
+            if (res.data.count % 5 !== 0) {
+              setCount(Math.floor(res.data.count / 5) + 1);
+            } else setCount(Math.floor(res.data.count / 5));
+          }
+          setData(res.data.results);
+          setSearchLoading(false);
+        })
+        .catch((err) => {
+          console.log(err.response);
+          setSearchLoading(false);
+        });
+    }
+  };
   const updateData = (pageNumber) => {
     let url = `http://13.92.195.8/api/users/?page=${pageNumber}`;
     setIsLoading(true);
@@ -31,7 +71,6 @@ const AdminNewAccounts = () => {
       })
       .then((res) => {
         setData(res.data.results);
-        setIsLoading(false);
       })
       .catch((err) => console.log(err.response));
   };
@@ -54,6 +93,7 @@ const AdminNewAccounts = () => {
         method: "get",
       })
       .then((res) => {
+        console.log(res);
         setData(res.data.results);
         setIsLoading(false);
         if (res.data.count % 5 !== 0) {
@@ -67,11 +107,17 @@ const AdminNewAccounts = () => {
       <div className="row">
         <div className="title_segment">
           <p className="extra-text text-default">New Accounts</p>
-          {!isLoading && data.length > 0 && (
-            <div className="number_new_accounts">
-              <span className="extra-small">{data.length}</span>
-            </div>
-          )}
+
+          <Search
+            loading={searchLoading}
+            className="search_table"
+            onSearchChange={handleSearch}
+            value={searchValue}
+            showNoResults={false}
+            results={null}
+            placeholder="Search for anything"
+            input={{ icon: "search", iconPosition: "left" }}
+          />
         </div>
       </div>
       <div className="row_t">
